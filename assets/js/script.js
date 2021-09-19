44ad1f6d1e2422497880c16245478bb2b32e5fe3
Tony Busch <abusch@css.edu>
2021-09-18 19:25:07 -0500

    add form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
-var buttonE1 = document.querySelector("#save-task");
+var formE1 = document.querySelector("#task-form");
 
 var createTaskHandler = function() {
+    Event.preventDefault();
     var taskItemE1 = document.createElement("li");
     taskItemE1.textContent = "This is a new task";
     taskItemE1.className = "task-item";
     tasksToDoE1.appendChild(taskItemE1);
 };
 
-buttonE1.addEventListener("click", createTaskHandler);
+formE1.addEventListener("click", createTaskHandler);
